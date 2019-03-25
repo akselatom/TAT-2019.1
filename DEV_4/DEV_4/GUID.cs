497fa1cfaa39ef57a9831120fd0c81988ead73f9
aksel <akcelatom@gmail.com>
2019-03-25 18:26:55 +0300

    GUID, Lecture development completed

--- DEV_4/DEV_4/GUID.cs
+++ DEV_4/DEV_4/GUID.cs
@@ -1,11 +1,74 @@
-﻿namespace DEV_4
+﻿
+namespace DEV_4
 {
+    /// <summary>
+    /// Globally Unique Identifier
+    /// </summary>
     public class GUID
     {
-        private static int callsNumber;
+        /// <summary>
+        /// Number of <see cref="GUID"/> objects. Required to generate default ID
+        /// </summary>
+        private static int callsNumber = 0;
 
-        private string text;
+        /// <summary>
+        /// The unique id.
+        /// </summary>
+        private string uniqueID;
 
+        /// <summary>
+        /// The description.
+        /// </summary>
         private string description;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GUID"/> class.
+        /// </summary>
+        public GUID()
+        {
+            this.GenerateUniqueID();
+            this.GenerateDescription();
+            callsNumber++;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GUID"/> class.
+        /// </summary>
+        /// <param name="id">
+        /// Unique identifier 
+        /// </param>
+        /// <param name="description">
+        /// The description.
+        /// </param>
+        public GUID(string id, string description = "no description")
+        {
+            this.GenerateUniqueID(id);
+            this.GenerateDescription(description);
+            callsNumber++;
+        }
+
+        /// <summary>
+        /// Unique index generator
+        /// </summary>
+        /// <param name="id">
+        /// The unique id.
+        /// </param>
+        private void GenerateUniqueID(string id = "object")
+        {
+            const int MaxStringSize = 257;
+            this.uniqueID = id.Length < MaxStringSize ? id + callsNumber : "object " + callsNumber;
+        }
+
+        /// <summary>
+        /// Description generator
+        /// </summary>
+        /// <param name="desc">
+        /// The escription
+        /// </param>
+        private void GenerateDescription(string desc = "no description")
+        {
+            const int MaxStringSize = 257;
+            this.description = desc.Length < MaxStringSize ? desc : "no description";
+        }
     }
 }
--- DEV_4/DEV_4/Materials.cs
+++ DEV_4/DEV_4/Materials.cs
@@ -1,24 +1,116 @@
 ﻿namespace DEV_4
 {
+    using System;
     using System.Collections.Generic;
+    using System.Linq;
 
+    /// <summary>
+    /// The materials.
+    /// </summary>
     public abstract class Materials
     {
+        /// <summary>
+        /// <see cref="GUID"/>
+        /// </summary>
         private GUID id;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Materials"/> class.
+        /// </summary>
+        protected Materials()
+        {
+            this.id = new GUID();
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Materials"/> class.
+        /// </summary>
+        /// <param name="id">
+        /// Unique identifier
+        /// </param>
+        /// <param name="description">
+        /// The description.
+        /// </param>
+        protected Materials(string id, string description = null)
+        {
+            this.id = description == null ? new GUID(id) : new GUID(id, description);
+        }
     }
 
-    public class Lection : Materials
+    /// <summary>
+    /// The lecture.
+    /// </summary>
+    public class Lecture : Materials
     {
-        private string lectionText;
+        /// <summary>
+        /// field containing lecture text.
+        /// </summary>
+        private string lectureText;
 
+        /// <summary>
+        /// <see cref="PresentationMaterial"/>
+        /// </summary>
         private PresentationMaterial materials;
+
+        /// <inheritdoc />
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:DEV_4.Lecture" /> class.
+        /// </summary>
+        public Lecture()
+        {
+            this.lectureText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.";
+            this.materials = new PresentationMaterial();
+        }
+
     }
 
+    /// <summary>
+    /// The presentation material.
+    /// </summary>
     public class PresentationMaterial : Materials
     {
-        private string URI;
+        
 
-        private string[] fileFormatsList = new string[2];
+        /// <summary>
+        /// The file formats list.
+        /// </summary>
+        private static readonly string[] FileFormatsList = { "unknown", "ppt", "pdf", "txt" };
+
+        /// <summary>
+        /// The uri.
+        /// </summary>
+        private string uri;
+
+        /// <summary>
+        /// The file format.
+        /// </summary>
+        private string fileFormat;
+
+        /// <inheritdoc />
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:DEV_4.PresentationMaterial" /> class.
+        /// </summary>
+        public PresentationMaterial()
+        {
+            this.uri = AppDomain.CurrentDomain.BaseDirectory + "\\PresentationMaterial\\lectureText";
+            this.fileFormat = FileFormatsList[3];
+        }
+
+        /// <inheritdoc />
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:DEV_4.PresentationMaterial" /> class.
+        /// </summary>
+        /// <param name="filePath">
+        /// The file path.
+        /// </param>
+        /// <param name="fileFormat">
+        /// The file format. <see cref="FileFormatsList"/>
+        /// </param>
+        public PresentationMaterial(string filePath, string fileFormat = "txt")
+        {
+            this.uri = filePath;
+            this.fileFormat = FileFormatsList.Contains(fileFormat) ? fileFormat : FileFormatsList[0];
+        }
     }
 
     public class Seminar : Materials
@@ -26,6 +118,7 @@
         private List<string> taskList;
 
         private List<string> answersList;
+      
     }
 
     public class LaboratoryWork : Materials

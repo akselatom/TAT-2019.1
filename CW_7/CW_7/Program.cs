b70a57b9e7c3167bf8246d11d9a7fdb330625997
aksel <akcelatom@gmail.com>
2019-05-13 21:37:49 +0300

    cw_7

---
+++ CW_7/CW_7/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CW_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FtpWorker newFtpWorker = new FtpWorker("ftp://ftp.byfly.by/", "test/");
+            Task[] downloadTask = new Task[newFtpWorker.GetFileNamesList().Count];
+            int i = 0;
+            foreach (var fileName in newFtpWorker.GetFileNamesList())
+            {
+                downloadTask[i] = new Task(() => newFtpWorker.DownloadFileFromFtpFolder(fileName));
+                i++;
+            }
+
+            foreach (var task in downloadTask)
+            {
+                task.Start();
+            }
+            Task.WaitAll(downloadTask);
+            Console.WriteLine("Job done");
+        }
+    }
+}
